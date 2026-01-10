79a31d8a408cc3b00c9ff14fb488b4282a7bf990
Lukáš Růžička (asus-haven-w) <ruzovej@gmail.com>
2026-01-11 00:26:36 +0100

    WIP add file_target

---
+++ build_cxx/common/include/build_cxx/common/file_target.hxx
@@ -0,0 +1,47 @@
+/*
+  Copyright 2025 Lukáš Růžička
+
+  This file is part of build_cxx.
+
+  build_cxx is free software: you can redistribute it and/or modify it under the
+  terms of the GNU Lesser General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+
+  build_cxx is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+  details.
+
+  You should have received a copy of the GNU Lesser General Public License along
+  with build_cxx. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <filesystem>
+#include <string_view>
+
+#include "build_cxx/common/abstract_target.hxx"
+
+namespace build_cxx::common {
+
+struct file_target : abstract_target {
+  explicit file_target(location const *const loc, std::string_view const name,
+                       std::string_view const *const raw_deps,
+                       std::size_t const num_deps);
+
+  virtual ~file_target() = default;
+
+  [[nodiscard]] modification_time_t last_modification_time() const override;
+
+private:
+  file_target(file_target const &) = delete;
+  file_target &operator=(file_target const &) = delete;
+  file_target(file_target &&) = delete;
+  file_target &operator=(file_target &&) = delete;
+
+  std::filesystem::path resolved_path;
+};
+
+} // namespace build_cxx::common
---
+++ build_cxx/common/src/file_target.cxx
@@ -0,0 +1,52 @@
+/*
+  Copyright 2025 Lukáš Růžička
+
+  This file is part of build_cxx.
+
+  build_cxx is free software: you can redistribute it and/or modify it under the
+  terms of the GNU Lesser General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+
+  build_cxx is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+  details.
+
+  You should have received a copy of the GNU Lesser General Public License along
+  with build_cxx. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "build_cxx/common/file_target.hxx"
+#include <chrono>
+
+namespace build_cxx::common {
+
+file_target::file_target(location const *const loc, std::string_view const name,
+                         std::string_view const *const raw_deps,
+                         std::size_t const num_deps)
+    : abstract_target{loc, name, raw_deps, num_deps} {
+  // TODO ... is it really wanted in this form?!
+  if (name[0] == '/') {
+    resolved_path = std::filesystem::path{name};
+  } else {
+    std::filesystem::path listFolder{loc->aFilename};
+    listFolder.remove_filename();
+
+    resolved_path = listFolder / std::filesystem::path{name};
+  }
+}
+
+abstract_target::modification_time_t
+file_target::last_modification_time() const {
+  try {
+    auto const ftime{
+        std::filesystem::last_write_time(resolved_path).time_since_epoch()};
+    return static_cast<modification_time_t>(
+        std::chrono::duration_cast<std::chrono::nanoseconds>(ftime).count());
+  } catch (std::filesystem::filesystem_error const &) {
+    return never_up_to_date;
+  }
+}
+
+} // namespace build_cxx::common
